d72fb7cd5247413ab445c21051814d15a9f9da8e
Pedero Wasto <123145577+pederowasto@users.noreply.github.com>
2023-01-25 00:57:46 +0700

    Update app.py
    add col 2

--- app.py
+++ app.py
@@ -16,4 +16,14 @@ labels = list(df['sentiment'].value_counts().index)
 explode = (0.1, 0, 0)
 ax.pie(x = sizes, labels = labels, autopct = '%1.1f%%', explode = explode, textprops={'fontsize': 4})
 ax.set_title('Distribution of Sentimen in Tokopedia tweet', fontsize = 8)
-st.pyplot(fig)
+
+col_1, col_2 = st.columns(2)
+
+with col_1:
+    st.pyplot(fig)
+   
+with col_2:
+    sns.barplot(df['sentiment'].value_counts().index,df['sentiment'].value_counts())
+    plt.title(f'Distribution Sentiment')
+    plt.xlabel('Labels')
+    plt.ylabel('Freq')
