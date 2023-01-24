a1f93cf058d0b6d51f3b573f2f3e2f04b1429d99
Pedero Wasto <123145577+pederowasto@users.noreply.github.com>
2023-01-25 01:17:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ st.write('### Tokopedia sentiment analysis')
 df = pd.read_csv('tokopedia_sentiment_analysis_clean.csv').reset_index(drop=True)
 st.dataframe(df)
 
-fig, ax = plt.subplots(figsize = (2, 2))
+fig, ax = plt.subplots(figsize = (4, 2))
 sizes = [count for count in df['sentiment'].value_counts()]
 labels = list(df['sentiment'].value_counts().index)
 explode = (0.1, 0, 0)
 ax.pie(x = sizes, labels = labels, autopct = '%1.1f%%', explode = explode, textprops={'fontsize': 4})
 ax.set_title('Distribution of Sentimen in Tokopedia tweet', fontsize = 8)
+
+fig2 = plt.figure(figsize=(2, 2))
+sns.countplot(x=df['sentiment'], data=df)
 
 col_1, col_2 = st.columns(2)
 
@@ -23,9 +26,4 @@ with col_1:
     st.pyplot(fig)
    
 with col_2:
-    fig2 = sns.barplot(df['sentiment'].value_counts().index,df['sentiment'].value_counts())
-    plt.title(f'Distribution Sentiment')
-    plt.xlabel('Labels')
-    plt.ylabel('Freq')
-    fig2.savefig('file.png')
     st.pyplot(fig2)
